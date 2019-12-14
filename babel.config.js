950a91f585385ab7c5de076e370ef558900a2726
Ravi Teja Gannavarapu <grt4004@gmail.com>
2019-12-15 01:22:58 +0530

    Updated package.json

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true)
   const presets = ["@babel/preset-env"]
-  const ignore = ["./node_modules"];
+  const ignore = ["./node_modules", "babel.config.js"];
 
   return {
     presets,
